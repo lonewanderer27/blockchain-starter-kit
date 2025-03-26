f7c8b911b37d07f77941c7c51034aba100308611
lonewanderer27 <adrianejames27@gmail.com>
2025-03-26 22:24:21 +0800

    feat(contracts): create social network

---
+++ src/contracts/SocialNetwork.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract SocialNetwork {
+    // State variable
+    string public name;
+
+    // Constructor function
+    constructor () public {
+        name = "Dapp University Social Network";
+    }
+}
